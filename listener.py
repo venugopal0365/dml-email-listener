6e8dadad5c538f31941ab01afb0e2a2a7f7ccb74
venugopal0365 <venu0365@gmail.com>
2026-04-16 17:02:52 +0530

    Update listener.py

--- listener.py
+++ listener.py
@@ -13,7 +13,7 @@ DB_HOST     = os.getenv("DB_HOST")
 DB_NAME     = os.getenv("DB_NAME")
 DB_USER     = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT     = os.getenv("DB_PORT")
+DB_PORT = int(os.getenv("DB_PORT"))
 
 # ─── EMAIL CONFIG ─────────────────────────────────────────────
 SMTP_HOST   = os.getenv("SMTP_HOST")
